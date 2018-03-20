6eef92abb1db71df5452aa82fc0162a855a55da8
Alexander <smlwmy@gmail.com>
2018-03-20 22:13:07 +0500

    start oop

--- chiphers/cezar.js
+++ chiphers/cezar.js
@@ -1,9 +1,6 @@
 // Шифр цезаря
 
-// var alphbet = 'аАбБвВгГдДеЕёЁжЖзЗиИйЙкКлЛмМнНоОпПрРсСтТуУфФхХцЦчЧшШщЩьЬъЪыЫэЭюЮяЯ';
-// var alphbet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var alphbet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя';
-
 
 var k, n;
 
@@ -12,26 +9,26 @@ n = alphbet.length - 1;
 
 function letsCezar() {
 
-    // var inputPhraze = document.getElementById('inputVal').value;
+    var openMessage = document.getElementById('inputVal').value; // Получаем значение из инпута
 
-    // this.openMessage = document.getElementById('inputVal').value;
-
-    var arr = openMessage.split('');
+    var arr = openMessage.split('');// разделяем его по символам
 
     var input = document.getElementById('input');
     var output = document.getElementById('output');
 
+
+    // выводи результат
     for(var i = 0; i <= arr.length-1; i++){
 
-        input.innerHTML += '<div class="letter">' + arr[i] + '</div>';
+        input.innerHTML += '<div class="letter">' + arr[i] + '</div>'; // выводим одну текущую букву
 
-        var currentLetter = arr[i];
+        var currentLetter = arr[i]; // Берем текущую букву
 
-        var newIndex = alphbet.search(currentLetter) + 1;
+        var newIndex = alphbet.search(currentLetter) + 1; // Ищем ее индекс в алфавите
 
-        var total = (newIndex + k ) % alphbet.length - 1;
+        var total = (newIndex + k ) % alphbet.length - 1; // Сдвигаем ее на 3
 
-        output.innerHTML += '<div class="letter">' + alphbet[total] + '</div>';
+        output.innerHTML += '<div class="letter">' + alphbet[total] + '</div>'; // выводим сдвинутую букву
     }
 }
 
---
+++ chiphers/main.js
@@ -0,0 +1,12 @@
+
+ /*
+    Created by smlw on 20.03.2018.
+    cezarr    
+*/
+
+
+var Main = {
+
+    alphbet : 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'
+
+};
