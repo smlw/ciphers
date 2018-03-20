8f95afe57e929029d58156ba5f43b2c8e06d8e08
Alexander <smlwmy@gmail.com>
2018-03-20 23:14:31 +0500

    Правочки

--- chiphers/podstanovka.js
+++ chiphers/podstanovka.js
@@ -1,33 +1,18 @@
 var alphbet = 'абвгдежзийклмнопрстуфхцчшщъыьэюя ';
 
-// function compareNumeric(a, b) {
-//     if (a > b) return 1;
-//     if (a < b) return -1;
-// }
-/*
-прик
-ладн
-аяма
-тема
-тик
+function vijener() {
 
+    var key = document.getElementById('inputKey').value;
+    var message = document.getElementById('inputMessage').value;
 
-пр__
-
-
- */
-
-function sub() {
-
-    var key = 'гвба';
-    // var message = 'Прикладная матем';
-    var message = 'пр'; // Прикл адная матем атик
     var test = message.split('');
+
+    var output = document.getElementById('output');
 
     var stroka = [];
     var keySort = [];
 
-    console.log(test);
+    output.innerHTML += ('<b>Разделенное по буквам сообщение:</b> <br>' + test + '<br>');
 
     // Ищем ключи букв алфавита в ключе
     for (var b = 0; b <= key.length - 1; b++) {
@@ -43,11 +28,15 @@ function sub() {
     var messageToKey = lenMessage / lenKey; // длину массива / длину ключа
     messageToKey = Math.ceil(messageToKey); // Вычисяем сколько будет строк
 
-    // добавием строку символом "_"
-    complementString = lenKey - (lenMessage%lenKey);
-    complementString = (lenMessage) + complementString;
+    // Проверяем, нужно ли добавлять пробелы строке
+    if(!(lenMessage%lenKey === 0)){
+        complementString = lenKey - (lenMessage%lenKey);
+        complementString = (lenMessage) + complementString;
 
-    message = message.padEnd(complementString, "_");
+        message = message.padEnd(complementString, "_");
+        output.innerHTML += ('<b>Сообщение с дополненныйми пробелами: </b> <br>' + message + '<br>');
+    }
+
 
     // Ищем индксы букв сообщения, что бы записать шифрованное сообщение в строку
     for (var i = 0; i < lenKey; i++) {
@@ -59,33 +48,35 @@ function sub() {
         }
     }
 
-    console.log(stroka);
+    output.innerHTML += ('<b>Зашифрованное сообщение:</b> <br>' + stroka + '<br>');
 
     // Делим строку на равные части
     pattern = new RegExp(".{1,"+messageToKey+"}", "g");
     var splitOutputMessage = stroka.match(pattern);
 
-    console.log(splitOutputMessage);
+    output.innerHTML += ('<b>Разделенно сообщение на части количество которых равно количеству букв шифра: </b> <br>' + splitOutputMessage + '<br>');
+
 
     // Заполняем двумерный массив [ключ буквы алфавита][кусок шифрованных букв]
     var doubleArray = [];
     for (var j = 0; j <= lenKey - 1; j++) {
         doubleArray.push([keySort[j], splitOutputMessage[j]]);
     }
+    output.innerHTML += ('<b>Не отсортированный массив</b> <br>' + doubleArray + '<br>');
+
 
     // Сортируем исходный массив по первому ключу (индекс буквы в алфавите)
     doubleArray.sort(function (a, b) {
         return a[0] - b[0]
     });
-    console.log(doubleArray);
+    output.innerHTML += ('<b>Отсортированный массив</b> <br>' + doubleArray + '<br>');
 
     // Заполняем массив исходного сообщения только шифрованными буквами
     var finalOutPutArray = [];
     for (var o = 0; o <= lenKey - 1; o++) {
         finalOutPutArray += (doubleArray[o][1]);
     }
-    console.log(finalOutPutArray.toLowerCase())
+
+    output.innerHTML += ('<b>Шифрованное сообщение</b> <br>' + finalOutPutArray.toLowerCase() + '<br>');
 
 }
-
-sub();
